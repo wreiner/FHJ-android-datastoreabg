3c48e88a986be3a75ca1e41620c24b45df503fa1
Walter Reiner <walter.reiner@wreiner.at>
2024-06-16 11:06:01 +0200

    Add logic to store/retrieve/clear input fields

--- app/src/main/java/com/example/datastoreabg/MainActivity.kt
+++ app/src/main/java/com/example/datastoreabg/MainActivity.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.datastoreabg.ui.theme.DataStoreAbgTheme
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +40,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DataStoreAbgTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
                 }
             }
         }
@@ -31,17 +52,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val context = LocalContext.current
+
+    var nameValue by remember { mutableStateOf("") }
+    var emailValue by remember { mutableStateOf("") }
+
+    var nameInput by remember { mutableStateOf("") }
+    var emailInput by remember { mutableStateOf("") }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(context) {
+        nameValue = SettingsDataStore.getNameFlow(context).first()
+        emailValue = SettingsDataStore.getEmailFlow(context).first()
+    }
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+        TextField(value = nameInput,
+            onValueChange = { nameInput = it },
+            label = {
+                Text(text = "Name")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(value = emailInput,
+            onValueChange = { emailInput = it },
+            label = {
+                Text(text = "Email")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row (
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    SettingsDataStore.saveName(context, nameInput)
+                    SettingsDataStore.saveEmail(context, emailInput)
+                }
+            }) {
+                Text("SAVE")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    nameInput = SettingsDataStore.getNameFlow(context).first()
+                    emailInput = SettingsDataStore.getEmailFlow(context).first()
+                }
+            }) {
+                Text("RETRIEVE")
+            }
+
+            Button(onClick = {
+                nameInput = ""
+                emailInput = ""
+            }) {
+                Text("CLEAR")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DataStoreAbgTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
