5e8eef74b1b6b8bdb8f5b9f982219f470fc53a8a
Walter Reiner <walter.reiner@wreiner.at>
2024-06-16 10:13:51 +0200

    Add datastore-preferences to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
